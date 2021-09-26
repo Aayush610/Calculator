ede58747f31da3e782571cc67dfbee6a7f253e09
Aayush Prakash <66993051+Sulphur06@users.noreply.github.com>
2021-09-26 15:15:06 +0530

    Add files via upload
    Calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+a = input("Choose a mathematical operator: ")
+
+first_number = int((input("Choose First Number: ")))
+second_number = int((input("Choose Second Number: ")))
++
+
+if a == "+":
+    print(first_number + second_number)
+    
+
+if a == "-":
+    print(first_number - second_number)
+
+if a == "/":
+    print(first_number / second_number)
+
+if a == "*":
+    print(first_number * second_number)
+
+
+
+
+  import time
+    print("Your answer is baking, so please wait a moment")
+    time.sleep(1.5)
