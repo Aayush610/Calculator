f999d3784b4f57f17913be9dc19e380b549cb03b
Aayush Prakash <66993051+Sulphur06@users.noreply.github.com>
2022-07-05 21:37:07 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,6 @@ a = input("Choose a mathematical operator: ")
 
 first_number = int((input("Choose First Number: ")))
 second_number = int((input("Choose Second Number: ")))
-+
 
 if a == "+":
     print(first_number + second_number)
@@ -20,6 +19,4 @@ if a == "*":
 
 
 
-  import time
-    print("Your answer is baking, so please wait a moment")
-    time.sleep(1.5)
+  
